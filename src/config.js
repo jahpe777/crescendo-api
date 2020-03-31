b9af3d0c1ec4e91019dc522778f8cbe2f25940b6
James Higgs <james.y.higgs@gmail.com>
2020-03-31 16:18:00 -0700

    200331-MVP-FINAL_2

--- src/config.js
+++ src/config.js
@@ -3,6 +3,9 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL:
     process.env.DATABASE_URL || 'postgresql://james@localhost/crescendo',
+  TEST_DATABASE_URL:
+    process.env.TEST_DATABASE_URL ||
+    'postgresql://dunder_mifflin@localhost/crescendo-test',
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY
 };
